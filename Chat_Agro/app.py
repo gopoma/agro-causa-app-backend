d8d4fce588da01dc77ed763b4f671ddc14fe1dab
Fluchi <fcardenasm@unsa.edu.pe>
2024-10-06 08:49:29 -0500

    Add: function load_database

--- Chat_Agro/app.py
+++ Chat_Agro/app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
 
 app = Flask(__name__)
 
+def load_database(persist_directory):
+    embedding = OpenAIEmbeddings()
+    vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+    return vectordb
 
 @app.route('/')
 def hello_world():  # put application's code here
